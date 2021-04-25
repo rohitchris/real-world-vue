e695835518d7c4a249d90591090d0a7561bf4989
Rohit D'Souza <rohit@tomorrownetworks.com>
2021-04-25 12:10:45 +0530

    editor settings

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1></h1>
+  <div>
+    <ul>
+      <li>hello there</li>
+    </ul>
+  </div>
+  <span><h1>Hello world</h1></span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'single quoted!',
+    }
+  },
+}
+</script>
+
+<style></style>
