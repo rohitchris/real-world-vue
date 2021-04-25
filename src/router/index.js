867da56d4a176ae9e2aac41f29527516bd92558b
Rohit D'Souza <rohit@tomorrownetworks.com>
2021-04-25 12:53:25 +0530

    routing tips

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,18 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'home',
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: '/about-us',
+    name: 'about',
+    alias: '/about',
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
 
